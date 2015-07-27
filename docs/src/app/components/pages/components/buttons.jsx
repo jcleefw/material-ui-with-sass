49ae1b38188ef6e62ed99dc3888797cdda57e38a
Kabir Sarin <sarink87@gmail.com>
2015-07-27 13:58:37 -0500

    Write some new documentation for the floating-action-button

--- docs/src/app/components/pages/components/buttons.jsx
+++ docs/src/app/components/pages/components/buttons.jsx
@@ -10,7 +10,18 @@ var ButtonPage = React.createClass({
 
   render: function() {
 
-    var code = 
+    var code =
+      '//material-ui-with-sass FloatingActionButton additions\n' +
+      '<FloatingActionButton text="face" useIconsForText={true} tooltip="I\'m a face!" tooltipPosition="left" />\n' +
+      '<FloatingActionButton text="KS" useIconsForText={false} secondary={true} />\n' +
+      'var speedDialButtons = [\n' +
+      '  {tooltip: "Previous", text: "skip_previous"},\n' +
+      '  {tooltip: "Play",     text: "play_arrow"},\n' +
+      '  {tooltip: "Next",     text: "skip_next", onClick: function(e) { console.log(e); }},\n' +
+      '  {tooltip: "abc",      text: "ABC", useIconsForText: false, secondary: true}\n' +
+      '];\n' +
+      '<FloatingActionButton useIconsForText={true} text="music_note" speedDialButtons={speedDialButtons} speedDialOpenText="keyboard_arrow_right" speedDialDirection="up" speedDialTransitionRotate={90} />\n\n' +
+
       '//Flat Buttons\n' +
       '<FlatButton label="Default" />\n' +
       '<FlatButton label="Primary" primary={true} />\n' +
@@ -26,6 +37,7 @@ var ButtonPage = React.createClass({
       '  </FlatButton>\n' +
       '</div>\n' +
       '<FlatButton label="Disabled" disabled={true} />\n\n' +
+
       '//Raised Buttons\n' + 
       '<RaisedButton label="Default" />\n' +
       '<RaisedButton label="Primary" primary={true} />\n' +
@@ -41,6 +53,7 @@ var ButtonPage = React.createClass({
       '  </RaisedButton>\n' +
       '</div>\n' +
       '<RaisedButton label="Disabled" disabled={true} />\n\n' +
+
       '//Floating Action Buttons\n' +
       '<FloatingActionButton iconClassName="muidocs-icon-action-grade" />\n' +
       '<FloatingActionButton iconClassName="muidocs-icon-action-grade" mini={true} />\n' +
@@ -48,7 +61,7 @@ var ButtonPage = React.createClass({
       '<FloatingActionButton iconClassName="muidocs-icon-custom-github" linkButton={true} href="https://github.com/sarink/material-ui-with-sass" mini={true} secondary={true}/>' +
       '<FloatingActionButton iconClassName="muidocs-icon-action-grade" mini={true} disabled={true} />\n' +
       '<FloatingActionButton iconClassName="muidocs-icon-action-grade" secondary={true} />\n' +
-      '<FloatingActionButton iconClassName="muidocs-icon-action-grade" mini={true} secondary={true} />';
+      '<FloatingActionButton iconClassName="muidocs-icon-action-grade" mini={true} secondary={true} />\n\n';
 
     var desc = 'This component generates a button element and all props except for ' +
       'the custom props below will be passed down to the button element. Also, ' +
@@ -147,9 +160,83 @@ var ButtonPage = React.createClass({
             type: 'bool',
             header: 'default: false',
             desc: 'If true, the button will use the secondary button colors.'
-          }
+          },
+          {
+            name: 'tooltip',
+            type: 'string',
+            header: 'optional',
+            desc: 'Adds a tooltip to the button'
+          },
+          {
+            name: 'tooltipPosition',
+            type: 'string',
+            header: 'optional (default: "left")',
+            desc: 'Specifies where the tooltip is anchored. Valid values are: top, right, bottom, left'
+          },
+          {
+            name: 'useSpeedDial',
+            type: 'bool',
+            header: 'default: false',
+            desc: 'Enables speed dial functionality'
+          },
+          {
+            name: 'text',
+            type: 'string',
+            header: 'default: ""',
+            desc: 'Adds some text to the button, will be an icon if useIconsForText is set to true'
+          },
+          {
+            name: 'useIconsForText',
+            type: 'bool',
+            header: 'default: true',
+            desc: 'Pass the "text" prop, and use any of the icon names in https://www.google.com/design/icons/'
+          },
+          {
+            name: 'speedDialButtons',
+            type: 'array',
+            header: 'optional (default: [])',
+            desc: 'An array of objects which are just props used to create each speed dial button. A speed dial button is nothing more than another FloatingActionButton with mini=true and useSpeedDial=false'
+          },
+
+          {
+            name: 'speedDialOpenText',
+            type: 'string',
+            header: 'optional (default: "keyboard_arrow_left")',
+            desc: 'When the speed dial buttons are shown, the main button will display this text (remember, with useIconsForText set to true, this is actually an icon)'
+          },
+          {
+            name: 'speedDialTransitionRotate',
+            type: 'number',
+            header: 'optional (default: 90)',
+            desc: 'Number of degrees the main button will rotate when the speed dial opens up'
+          },
+          {
+            name: 'speedDialTransitionTime',
+            type: 'number',
+            header: 'optional (default: 50)',
+            desc: 'Milliseconds the speed dial will spend rotating'
+          },
+          {
+            name: 'speedDialVisibleTime',
+            type: 'number',
+            header: 'optional (default: 2000)',
+            desc: 'Milliseconds to delay the speed dial after mouseout before closing'
+          },
+          {
+            name: 'speedDialDirection',
+            type: 'string',
+            header: 'optional (default: "up")',
+            desc: 'Specifies which direction the speed dial will open, currently only "up" is supported'
+          },
         ]
       }
+    ];
+
+    var speedDialButtons = [
+      {tooltip: "Previous", text: "skip_previous"},
+      {tooltip: "Play",     text: "play_arrow"},
+      {tooltip: "Next",     text: "skip_next", onClick: function(e) { console.log(e); }},
+      {tooltip: "ABC",      text: "ABC", useIconsForText: false, secondary: true}
     ];
 
     return (
@@ -163,7 +250,22 @@ var ButtonPage = React.createClass({
 
           <div className="button-example-group">
             <div className="button-example-container">
-              <FlatButton label="Default" />
+              <FloatingActionButton text="face" useIconsForText={true} tooltip="I'm a face!"/>
+            </div>
+            <div className="button-example-container">
+              <FloatingActionButton text="KS" useIconsForText={false} secondary={true} />
+            </div>
+            <div className="button-example-container">
+              <FloatingActionButton text="music_note" speedDialButtons={speedDialButtons} useSpeedDial={true} />
+            </div>
+          </div>
+
+
+
+
+          <div className="button-example-group">
+            <div className="button-example-container">
+              <FlatButton label="Default" tooltip="Default" tooltipPosition="left" />
             </div>
             <div className="button-example-container">
               <FlatButton label="Primary" primary={true} />
@@ -232,16 +334,6 @@ var ButtonPage = React.createClass({
               <FloatingActionButton iconClassName="muidocs-icon-action-grade" mini={true} disabled={true} />
             </div>
           </div>
-
-          <div className="button-example-group">
-            <div className="button-example-container">
-              <FloatingActionButton iconClassName="muidocs-icon-action-grade" secondary={true} />
-            </div>
-            <div className="button-example-container">
-              <FloatingActionButton iconClassName="muidocs-icon-action-grade" mini={true} secondary={true} />
-            </div>
-          </div>
-
         </div>
 
       </ComponentDoc>
