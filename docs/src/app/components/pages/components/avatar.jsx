6946786d995c12fa48ecc27721e331e5e86f106c
Kabir Sarin <sarink87@gmail.com>
2015-07-29 17:36:08 -0500

    Update Avatar docs

--- docs/src/app/components/pages/components/avatar.jsx
+++ docs/src/app/components/pages/components/avatar.jsx
@@ -6,16 +6,16 @@ var ComponentDoc = require('../../component-doc.jsx');
 
 var AvatarPage = React.createClass({
   render: function() {
-    var avatarCode = '<Avatar className="my-custom-avatar">KS</Avatar>\n\n' +
+    var avatarCode = '<Avatar className="my-custom-avatar">KS</Avatar>\n' +
+                     '/* in your css /* .my-custom-avatar { background-color: red; }\n\n' +
                      '<Avatar><Icon>face</Icon></Avatar>\n\n' +
-                     '// in your css...\n' +
-                     '.my-custom-avatar { background-color: red; }';
+                     '<Avatar><img src="images/partly_cloudy.png" /></Avatar>\n\n';
     var avatarDesc = (
       <p className="mui-font-style-subhead-1">
-        The Avatar component is very simple. Basically, it renders a circle with some centered content inside of it.<br/>
-        You can put anything inside of it, and it can be styled with regular css.<br/>
-        The default size is 40px, but you can set your own width/height in css, or in inline-styles if you like.<br/>
-        You can embed an Icon, an img, or just some plain old text.
+        The Avatar component is very simple. Basically, it renders a circle with some centered content inside of it.<br/><br/>
+        The default size is 40px, but you can set your own width/height in css, or in inline-styles if you like.<br/><br/>
+        You can embed an Icon, an img, or just some plain old text.<br/><br/>
+        If it is an image, it will be center/clipped inside the circle.
       </p>
     );
 
@@ -31,6 +31,8 @@ var AvatarPage = React.createClass({
           <Avatar style={{backgroundColor: "red"}}>KS</Avatar>
           <br/>
           <Avatar><Icon>face</Icon></Avatar>
+          <br/>
+          <Avatar><img src="images/partly_cloudy.png" /></Avatar>
         </ComponentDoc>
       </div>
     );
